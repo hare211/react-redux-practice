2ecf63ed52c51d8fa82d10dde8beb33c932cdc93
hare211 <blackhole2748@gmail.com>
2025-05-27 14:23:33 +0900

    main page 작성

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -6,7 +6,7 @@ function Home() {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetchMainData());
-    }, []) // vue -> mounted()
+    }, []);
 
     const mainData = useSelector(state => state.mains.main_data);
     return (
@@ -16,37 +16,32 @@ function Home() {
                     <div className="row">
                         <div className="col-12 col-md-6 col-lg-4">
                             <div className="single_catagory wow fadeInUp" data-wow-delay=".3s">
-                                <img src="img/catagory-img/1.jpg" alt=""/>
+                                <img src="img/catagory-img/1.jpg" alt="" />
                                 <div className="catagory-title">
-                                    <a href="#">
-                                        <h5>부산 맛집</h5>
-                                    </a>
+                                    <a href="#"><h5>부산 맛집</h5></a>
                                 </div>
                             </div>
                         </div>
                         <div className="col-12 col-md-6 col-lg-4">
                             <div className="single_catagory wow fadeInUp" data-wow-delay=".6s">
-                                <img src="img/catagory-img/2.jpg" alt=""/>
+                                <img src="img/catagory-img/2.jpg" alt="" />
                                 <div className="catagory-title">
-                                    <a href="#">
-                                        <h5>부산 동영상</h5>
-                                    </a>
+                                    <a href="#"><h5>부산 동영상</h5></a>
                                 </div>
                             </div>
                         </div>
                         <div className="col-12 col-md-6 col-lg-4">
                             <div className="single_catagory wow fadeInUp" data-wow-delay=".9s">
-                                <img src="img/catagory-img/3.jpg" alt=""/>
+                                <img src="img/catagory-img/3.jpg" alt="" />
                                 <div className="catagory-title">
-                                    <a href="#">
-                                        <h5>부산 명소</h5>
-                                    </a>
+                                    <a href="#"><h5>부산 명소</h5></a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
+
             <section className="blog_area section_padding_0_80">
                 <div className="container">
                     <div className="row justify-content-center">
@@ -55,45 +50,185 @@ function Home() {
                                 <div className="col-12">
                                     <div className="single-post wow fadeInUp" data-wow-delay=".2s">
                                         <div className="post-thumb">
-                                            <img src={ mainData.mp && "https://www.menupan.com" + mainData.mp.poster} alt=""/>
+                                            <img src={mainData?.mp && "https://www.menupan.com" + mainData.mp.poster} alt="" />
                                         </div>
                                         <div className="post-content">
                                             <div className="post-meta d-flex">
                                                 <div className="post-author-date-area d-flex">
                                                     <div className="post-author">
-                                                        <a href="#">{ mainData.mp && mainData.mp.type }</a>
+                                                        <a href="#">{mainData?.mp?.type}</a>
                                                     </div>
                                                     <div className="post-date">
-                                                        <a href="#">{ mainData.mp && mainData.mp.score }</a>
+                                                        <a href="#">{mainData?.mp?.score}</a>
                                                     </div>
                                                 </div>
                                                 <div className="post-comment-share-area d-flex">
                                                     <div className="post-favourite">
-                                                        <a href="#"><i className="fa fa-heart-o" aria-hidden="true"></i>{ mainData.mp && mainData.mp.likecount }</a>
+                                                        <a href="#"><i className="fa fa-heart-o" aria-hidden="true"></i>{mainData?.mp?.likecount}</a>
                                                     </div>
                                                     <div className="post-comments">
-                                                        <a href="#"><i className="fa fa-comment-o" aria-hidden="true"></i> { mainData.mp && mainData.mp.hit }</a>
+                                                        <a href="#"><i className="fa fa-comment-o" aria-hidden="true"></i>{mainData?.mp?.hit}</a>
                                                     </div>
                                                     <div className="post-share">
                                                         <a href="#"><i className="fa fa-share-alt" aria-hidden="true"></i></a>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <a href="#">
-                                                <h2 className="post-headline">{ mainData.mp && mainData.mp.name }</h2>
-                                            </a>
-                                            <p>{ mainData.mp && mainData.mp.theme }</p>
+                                            <a href="#"><h2 className="post-headline">{mainData?.mp?.name}</h2></a>
+                                            <p>{mainData?.mp?.theme}</p>
                                             <a href="#" className="read-more">Continue Reading..</a>
                                         </div>
                                     </div>
                                 </div>
+
+                                {mainData?.mList?.map((food, idx) => (
+                                    <div className="col-12 col-md-6" key={idx}>
+                                        <div className="single-post wow fadeInUp" data-wow-delay=".4s">
+                                            <div className="post-thumb">
+                                                <img src={"http://www.menupan.com" + food.poster} alt="" />
+                                            </div>
+                                            <div className="post-content">
+                                                <div className="post-meta d-flex">
+                                                    <div className="post-author-date-area d-flex">
+                                                        <div className="post-author">
+                                                            <a href="#">{food.type}</a>
+                                                        </div>
+                                                        <div className="post-date">
+                                                            <a href="#">{food.score}</a>
+                                                        </div>
+                                                    </div>
+                                                    <div className="post-comment-share-area d-flex">
+                                                        <div className="post-favourite">
+                                                            <a href="#"><i className="fa fa-heart-o" aria-hidden="true"></i>{food.likecount}</a>
+                                                        </div>
+                                                        <div className="post-comments">
+                                                            <a href="#"><i className="fa fa-comment-o" aria-hidden="true"></i>{food.hit}</a>
+                                                        </div>
+                                                        <div className="post-share">
+                                                            <a href="#"><i className="fa fa-share-alt" aria-hidden="true"></i></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <a href="#"><h4 className="post-headline">{food.name}</h4></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+
+                                {mainData?.lList?.map((food, idx) => (
+                                    <div className="col-12" key={idx}>
+                                        <div className="list-blog single-post d-sm-flex wow fadeInUpBig" data-wow-delay=".1s">
+                                            <div className="post-thumb">
+                                                <img src={"http://www.menupan.com" + food.poster} alt="" />
+                                            </div>
+                                            <div className="post-content">
+                                                <div className="post-meta d-flex">
+                                                    <div className="post-author-date-area d-flex">
+                                                        <div className="post-author">
+                                                            <a href="#">{food.type}</a>
+                                                        </div>
+                                                        <div className="post-date">
+                                                            <a href="#">{food.score}</a>
+                                                        </div>
+                                                    </div>
+                                                    <div className="post-comment-share-area d-flex">
+                                                        <div className="post-favourite">
+                                                            <a href="#"><i className="fa fa-heart-o" aria-hidden="true"></i>{food.likecount}</a>
+                                                        </div>
+                                                        <div className="post-comments">
+                                                            <a href="#"><i className="fa fa-comment-o" aria-hidden="true"></i>{food.hit}</a>
+                                                        </div>
+                                                        <div className="post-share">
+                                                            <a href="#"><i className="fa fa-share-alt" aria-hidden="true"></i></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <a href="#"><h4 className="post-headline">{food.name}</h4></a>
+                                                <p>{food.address}</p>
+                                                <a href="#" className="read-more">Continue Reading..</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
                             </div>
                         </div>
-                    </div>
+                                <div className="col-12 col-sm-8 col-md-6 col-lg-4">
+                                    <div className="blog-sidebar mt-5 mt-lg-0">
+                                        <div className="single-widget-area about-me-widget text-center">
+                                            <div className="widget-title">
+                                                <h6>About Me</h6>
+                                            </div>
+                                            <div className="about-me-widget-thumb">
+                                                <img src="img/about-img/1.jpg" alt="" />
+                                            </div>
+                                            <h4 className="font-shadow-into-light">Shopia Bernard</h4>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+                                        </div>
+
+                                        <div className="single-widget-area subscribe_widget text-center">
+                                            <div className="widget-title">
+                                                <h6>Subscribe &amp; Follow</h6>
+                                            </div>
+                                            <div className="subscribe-link">
+                                                <a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a>
+                                                <a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+                                                <a href="#"><i className="fa fa-google" aria-hidden="true"></i></a>
+                                                <a href="#"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                                                <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+                                                <a href="#"><i className="fa fa-vimeo" aria-hidden="true"></i></a>
+                                            </div>
+                                        </div>
+
+                                        <div className="single-widget-area popular-post-widget">
+                                            <div className="widget-title text-center">
+                                                <h6>Popular Post</h6>
+                                            </div>
+                                            {[1, 2, 3, 4, 5].map(i => (
+                                                <div className="single-populer-post d-flex" key={i}>
+                                                    <img src={`img/sidebar-img/${i}.jpg`} alt="" />
+                                                    <div className="post-content">
+                                                        <a href="#"><h6>Sample Title {i}</h6></a>
+                                                        <p>Tuesday, October 3, 2017</p>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                        <div className="single-widget-area add-widget text-center">
+                                            <div className="add-widget-area">
+                                                <img src="img/sidebar-img/6.jpg" alt="" />
+                                                <div className="add-text">
+                                                    <div className="yummy-table">
+                                                        <div className="yummy-table-cell">
+                                                            <h2>Cooking Book</h2>
+                                                            <p>Buy Book Online Now!</p>
+                                                            <a href="#" className="add-btn">Buy Now</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="single-widget-area newsletter-widget">
+                                            <div className="widget-title text-center">
+                                                <h6>Newsletter</h6>
+                                            </div>
+                                            <p>Subscribe our newsletter for updates and discounts.</p>
+                                            <div className="newsletter-form">
+                                                <form action="#" method="post">
+                                                    <input type="email" name="newsletter-email" id="email" placeholder="Your email" />
+                                                    <button type="submit">
+                                                        <i className="fa fa-paper-plane-o" aria-hidden="true"></i>
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                 </div>
             </section>
         </>
-    )
+    );
 }
 
 export default Home;
