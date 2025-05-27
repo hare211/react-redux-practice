147f7ab59a79d32d68910ef97adceb0dde64a3fe
hare211 <blackhole2748@gmail.com>
2025-05-27 10:30:21 +0900

    push test

---
+++ src/actions/mainActions.js
@@ -0,0 +1,10 @@
+// Controller 저장 -> 메서드 -> 구분해서 처리...
+
+/*
+    Action: 데이터 CRUD -> 서버 연결
+        1) 어떤 기능이 있는지 설계
+            types.js
+            export const AA = 'AA' -> 내부 프로토콜
+            -> public final static int LOGIN = 100
+
+ */
