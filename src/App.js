ba6a27a33a75b50b806fe2e0f17ff58d74d5d512
hare211 <blackhole2748@gmail.com>
2025-05-27 11:52:18 +0900

    combineReducer 작성, App 에 store 작성(Provider)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/main/Home";
 import Header from "./components/main/Header";
 import Footer from "./components/main/Footer";
+import store from "./store/store";
+import { Provider } from "react-redux"; // 모든 곳에 store 를 사용 가능.
 
 function App() {
   return (
+      <Provider store={store}>
       <Router>
         <Header />
           <Routes>
@@ -12,6 +15,7 @@ function App() {
           </Routes>
         <Footer />
       </Router>
+      </Provider>
   );
 }
 
